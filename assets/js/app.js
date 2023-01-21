ab2e431f8a9dbf81091e23af86e9cc51b190b2cb
Esdras-Palacios <Esdras.palacios07@Gmail.com>
2023-01-20 23:20:42 -0600

    ejercicio resuelto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
+var error;
 
-function displayUser(username) {
+
+
+async function displayUser(username) {
+
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const response = await fetch(`${usersEndpoint}/${username}`).then(res =>res.json());
+  var data = JSON.stringify(response);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = response.name;
+  $b.textContent = response.blog;
+  $l.textContent = response.location;
+  
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  error = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
